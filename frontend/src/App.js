44085ee3753669c12b9f245e197c9fdb04d924ba
Thanusree Vuppu <thanusreevuppu025@gmail.com>
2025-11-19 13:06:27 +0530

    created index.js and App.js file

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EventGeniusChat from "./components/EventGeniusChat/EventGeniusChat";
+
+function App() {
+  return (
+    <div>
+      <h1>Giddly AI</h1>
+      <EventGeniusChat />
+    </div>
+  );
+}
+
+export default App;
